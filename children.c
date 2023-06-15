1e1be34f8bff6b2a01f9d7206ce53d368fb2b38d
bbeltran <bbeltran@student.42madrid.com>
2023-06-15 13:05:02 +0200

    Too many comments

--- children.c
+++ children.c
@@ -6,32 +6,43 @@
 /*   By: bbeltran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 17:28:17 by bbeltran          #+#    #+#             */
-/*   Updated: 2023/06/14 16:48:33 by bbeltran         ###   ########.fr       */
+/*   Updated: 2023/06/15 12:54:41 by bbeltran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void	child1_pr(t_pipex pipex, char **envp)
+/*void	child1_pr(t_pipex pipex, char **argv, char **envp)
 {
-	pipex.child1 = fork();
-	if (pipex.child1 == 0)
-	{
-		printf("child 1 process\n");
-		close(pipex.pipefd[0]);	
-		dup2(pipex.readfile, STDOUT_FILENO);
-		execve(pipex.cpath, pipex.cmds, envp);
-		close(pipex.pipefd[1]);
-	}
-	else
-	{
-		printf("parent 1 process\n");
-		close(pipex.pipefd[1]);
-	}
-	waitpid(pipex.child1, NULL, 0);
+	printf("child 1 process\n");
+	pipex.cmds = ft_split(argv[2], ' ');
+	pipex.cpath = get_commandpath(pipex, pipex.cmds[0]); 
+	close(pipefd[0]); //We won't need the read end of the pipe
+	dup2(pipex.readfile, 1);
+	execve(pipex.cpath, pipex.cmds, envp);
+	close(pipex.pipefd[1]);
+}*/
+void	child1_pr(t_pipex pipex, char **argv, char **envp)
+{
+	printf("child 1 process\n");
+	pipex.cmds = ft_split(argv[2], ' ');
+	pipex.cpath = get_commandpath(pipex, pipex.cmds[0]);
+	close(pipex.pipefd[0]);
+	dup2(pipex.pipefd[1], 1);
+	dup2(pipex.readfile, 0);
+	execve(pipex.cpath, pipex.cmds, envp);
+	close(pipex.pipefd[1]);
 }
+void	child2_pr(t_pipex pipex, char **argv, char **envp)
+{
+	printf("child 2 process\n");
+	pipex.cmds = ft_split(argv[3], ' ');
+	pipex.cpath = get_commandpath(pipex, pipex.cmds[0]);
+	close(
 
-void	child2_pr(t_pipex pipex, char **envp)
+	
+}
+/*void	child2_pr(t_pipex pipex, char **envp)
 {
 	if (pipex.child2 == 0)
 	{
@@ -47,4 +58,4 @@ void	child2_pr(t_pipex pipex, char **envp)
 		close(pipex.pipefd[0]);
 	}
 	waitpid(pipex.child2, NULL, 0);
-}
+}*/
--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: bbeltran <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/09 13:27:46 by bbeltran          #+#    #+#             */
-/*   Updated: 2023/06/14 17:53:46 by bbeltran         ###   ########.fr       */
+/*   Updated: 2023/06/15 13:03:27 by bbeltran         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,49 @@
 //	child2_pr(pipex, envp);
 	return (0);
 }*/
+/*int	main(int argc, char **argv, char **envp)
+{
+	if (argc != 5)
+	{
+		ft_putstr_fd("Usage: ./pipex readfile \"command1\" \"command2\" writefile\n", 1);
+		exit (0);
+	}
+	execute_p(argv, envp);
+}*/
+
+/*int	main(int argc, char **argv, char **envp)
+{
+	t_pipex	pipex;
+	int		bytesread;
+	char	c;
+
+	bytesread = 1;
+	pipex.PATH = get_path(envp);
+	pipex.readfile = open(argv[1], O_RDONLY, 0777);
+	pipe(pipex.pipefd);
+	pipex.child1 = fork();
+	if (pipex.child1 == 0)
+	{
+		printf("child 1 process\n");
+		close(pipex.pipefd[0]); //close read end of the pipe
+		dup2(pipex.readfile, 0); //change STDOUT to STDIN for command1
+		while (bytesread != EOF) //read the file and write it into the STDIN
+		{
+			read(pipex.readfile, &c, bytesread);
+			write(0, &c, 1);
+		}
+		dup2(pipex.pipefd[1], 1); //change STDIN[now 0] into the write end of the pipe
+		execve(pipex.cpath, pipex.cmds, envp); //execute command1
+		close(pipex.pipefd[1]); //we no longer need the pipe's write end
+	}
+	else
+	{
+		printf("Parent 1 process");
+		close(pipex.pipefd[1]);
+	}
+	waitpid(pipex.child1, NULL, 0);
+
+}*/
 int	main(int argc, char **argv, char **envp)
 {
 	if (argc != 5)
@@ -44,6 +87,43 @@ int	main(int argc, char **argv, char **envp)
 	}
 	execute_p(argv, envp);
 }
+
+void	execute_p(char **argv, char **envp)
+{
+	t_pipex	pipex;
+	
+	pipex.PATH = get_path(envp);
+	pipex.readfile = open(argv[1], O_RDONLY, 0777);
+	pipex.writefile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if (pipex.readfile == -1 || pipex.writefile == -1)
+		exit_error("File\n", -1);
+	if (pipe(pipex.pipefd) == -1) // create pipe and check for errors
+	{
+		close(pipex.pipefd);
+		exit_error("Pipe\n", -1);
+	}
+//	pipex.cmds = ft_split(argv[2], ' ');
+	pipex.child1 = fork();
+	if (pipex.child1 == 0)
+		child1_pr(pipex, argv, envp);
+	else
+	{
+		child2_pr(pipex, argv, envp);
+	}
+}
+
+/*void	read_file(t_pipex pipex)
+{
+	int		bytesread;
+	char	c;
+
+	bytesread = 1;
+	while (bytesread != EOF)
+	{
+		read(pipex.readfile, &c, bytesread);
+		write(0, &c, 1);
+	}
+}*/
 
 void	execute_p(char **argv, char **envp)
 {
@@ -63,13 +143,4 @@ void	execute_p(char **argv, char **envp)
 	child1_pr(pipex, envp);
 // 2nd child process
 	pipex.PATH = get_path(envp);
-}
-
-void	read_my_file(char **argv, int fd, t_pipex pipex)
-{
-	char	*wasread;
-
-	wasread = get_next_line(fd);
-
-
 }
